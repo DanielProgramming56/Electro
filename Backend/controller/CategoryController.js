6d334bf1ebc700c54caaa622909506d86ba83dcc
daniel.chidebere <daniel.chidebere@konga.com>
2023-11-08 17:48:24 +0100

    category

--- Backend/controller/CategoryController.js
+++ Backend/controller/CategoryController.js
@@ -13,3 +13,38 @@ export const getCategories = async (req, res, next) => {
         next(error)
     }
 }
+
+export const newCategory = async (req, res, next) => {
+    try {
+        const { category } = req.body
+        if (!category) {
+            res.status(400).send("Category input is required")
+        }
+        const categoryExist = await Category.find({ name: category })
+        if (categoryExist) {
+            res.status(400).send("Categoty already exists")
+        }
+        else {
+            const categoryCreated = await Category.create({
+                name: category
+            })
+            res.status(201).send("created category")
+        }
+    } catch (error) {
+
+    }
+}
+
+export const deleteCategory = async (req, res, next) => {
+    try {
+        if (req.params.category != "Choose category") {
+            const categoryExist = await Category.findOne({
+                name: decodeURIComponent(req.params.category)
+            }).orFail()
+            await categoryExist.remove()
+            res.json({ categoryDeleted: true })
+        }
+    } catch (error) {
+        next(error )
+    }
+}
--- Backend/routes/categoryRoutes.js
+++ Backend/routes/categoryRoutes.js
@@ -1,7 +1,8 @@
 import express from "express"
 const router = express.Router()
-import { getCategories } from "../controller/CategoryController.js"
+import { deleteCategory, getCategories, newCategory } from "../controller/CategoryController.js"
 
 router.get("/", getCategories)
-
+router.post("/", newCategory)
+router.post("/:category", deleteCategory)
 export default router
