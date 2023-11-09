18156a96a863541b613c76a8da4caca427f1b2b7
daniel.chidebere <daniel.chidebere@konga.com>
2023-11-09 15:47:30 +0100

    done(product)

--- Backend/controller/ProductController.js
+++ Backend/controller/ProductController.js
@@ -1,3 +1,50 @@
+import Product from "../Models/ProductModel.js"
+import { recordsPerPage } from "../config/pagination"
 export const getProducts = async (req, res, next) => {
-    res.send("Name")
+    try {
+        let query = {};
+        let queryCondition = false
+
+        let priceQueryCondition = {};
+        if (req.query.price) {
+            queryCondition = true;
+            priceQueryCondition = { price: { $lte: Number(req.query.price) } };
+        }
+        let ratingQueryCondition = {};
+        if (req.query.rating) {
+            queryCondition = true;
+            ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
+        }
+
+        if (queryCondition) {
+            query = {
+                $and: [priceQueryCondition, ratingQueryCondition],
+            };
+        }
+
+        //pagination
+        const pageNum = Number(req.query.pageNum) || 1;
+
+        // sort by name, price etc.
+        let sort = {};
+        const sortOption = req.query.sort || "";
+        if (sortOption) {
+            let sortOpt = sortOption.split("_");
+            sort = { [sortOpt[0]]: Number(sortOpt[1]) };
+        }
+
+        const totalProducts = await Product.countDocuments(query);
+        const products = await Product.find(query)
+            .skip(recordsPerPage * (pageNum - 1))
+            .sort(sort)
+            .limit(recordsPerPage);
+
+        res.json({
+            products,
+            pageNum,
+            paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
+        });
+    } catch (error) {
+        next(error);
+    }
 } 
--- Backend/routes/categoryRoutes.js
+++ Backend/routes/categoryRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
 const router = express.Router()
-import { deleteCategory, getCategories, newCategory } from "../controller/CategoryController.js"
+import { deleteCategory, getCategories, newCategory, saveAttr } from "../controller/CategoryController.js"
 
 router.get("/", getCategories)
 router.post("/", newCategory)
 router.post("/:category", deleteCategory)
+router.post("/attr", saveAttr)
 export default router
